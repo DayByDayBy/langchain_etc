5367539bb1944376d08e1238f055f85980dff2a1
Diomedes Git <diomedesgit@gmail.com>
2024-04-02 23:24:53 +0100

    more bits

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 function HomeScreen() {
   return (
@@ -24,9 +25,28 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'magic') {
+              iconName = focused
+                ? 'sparkles'
+                : 'sparkles-outline';
+            } else if (route.name === 'woah') {
+              iconName = focused ? 'speedometer' : 'speedometer-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="magic" component={HomeScreen} />
+        <Tab.Screen name="woah" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
